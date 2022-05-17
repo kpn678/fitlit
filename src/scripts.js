a00b9ad2b29ca9630abcb9baa52e9d90073ff130
Kristy Nguyen <knyen008@gmail.com>
2022-05-17 16:44:50 -0600

    Add stride length to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -14,11 +14,11 @@ const welcomeMessage = document.querySelector('h2');
 const stepGoalDisplay = document.querySelector('p');
 
 const generateWelcomeMessage = () => {
-  welcomeMessage.innerText = `Welcome, ${randomUser.returnUserFirstName()}`;
+  welcomeMessage.innerText = `Welcome back, ${randomUser.returnUserFirstName()}!`;
 };
 
 const displayStepGoal = () => {
-  stepGoalDisplay.innerHTML = `The average of all our users' daily step goal is: ${userRepository.calculateAverageStepGoals()} steps. <br> Your daily step goal is: ${randomUser.dailyStepGoal} steps.`
+  stepGoalDisplay.innerHTML = `The average of all our users' daily step goals is: ${userRepository.calculateAverageStepGoals()} steps. <br> Your daily step goal is: ${randomUser.dailyStepGoal} steps. <br> Your stride length is: ${randomUser.strideLength} feet.`
 };
 
 window.addEventListener('load', (event) => {
@@ -29,5 +29,6 @@ window.addEventListener('load', (event) => {
 // userData is an array of objects
 // userRepository is an object with all of the users in an array
 
-console.log("Single User Step Goal:", randomUser.dailyStepGoal);
-console.log("All Users' Average Step Goals:", userRepository.calculateAverageStepGoals());
+console.log(randomUser.returnFriendName(userData));
+console.log(randomUser.email);
+console.log(randomUser.address);
