85034e23b2effdcbd94c1250d256e6d83a2cc744
Kristy Nguyen <knyen008@gmail.com>
2022-05-17 14:52:30 -0600

    Add information for step goal display

--- src/scripts.js
+++ src/scripts.js
@@ -11,14 +11,23 @@ const randomUser = new User(userData[Math.floor(Math.random() * userData.length)
 console.log(randomUser);
 
 const welcomeMessage = document.querySelector('h2');
+const stepGoalDisplay = document.querySelector('p');
 
 const generateWelcomeMessage = () => {
   welcomeMessage.innerText = `Welcome, ${randomUser.returnUserFirstName()}`;
 };
 
+const displayStepGoal = () => {
+  stepGoalDisplay.innerHTML = `The average of all our users' daily step goal is: ${userRepository.calculateAverageStepGoals()} steps. <br> Your daily step goal is: ${randomUser.dailyStepGoal} steps.`
+};
+
 window.addEventListener('load', (event) => {
   generateWelcomeMessage();
+  displayStepGoal();
 });
 
 // userData is an array of objects
 // userRepository is an object with all of the users in an array
+
+console.log("Single User Step Goal:", randomUser.dailyStepGoal);
+console.log("All Users' Average Step Goals:", userRepository.calculateAverageStepGoals());
