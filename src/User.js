cfafa9249d98deec366115eaac98bbd1d8cd8ee0
Kristy Nguyen <knyen008@gmail.com>
2022-05-17 14:22:35 -0600

    Edit spacing

--- src/User.js
+++ src/User.js
@@ -1,21 +1,18 @@
 class User {
-    constructor(singleUserData) {
-        this.id = singleUserData.id;
-        this.name = singleUserData.name;
-        this.address = singleUserData.address;
-        this.email = singleUserData.email;
-        this.strideLength = singleUserData.strideLength;
-        this.dailyStepGoal = singleUserData.dailyStepGoal;
-        this.friends = singleUserData.friends;
-
-
-    }
-
-    returnUserFirstName() {
-        const splitName = this.name.split(" ");
-        return splitName[0];
-      }
-}
+  constructor(singleUserData) {
+    this.id = singleUserData.id;
+    this.name = singleUserData.name;
+    this.address = singleUserData.address;
+    this.email = singleUserData.email;
+    this.strideLength = singleUserData.strideLength;
+    this.dailyStepGoal = singleUserData.dailyStepGoal;
+    this.friends = singleUserData.friends;
+  };
+  returnUserFirstName() {
+    const splitName = this.name.split(" ");
+    return splitName[0];
+  };
+};
 
 
 
