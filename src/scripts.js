3b4ace2df48e3d498437370b4cca9749508bb413
Kristy Nguyen <knyen008@gmail.com>
2022-05-17 13:59:44 -0600

    Create randomize user instance

--- src/scripts.js
+++ src/scripts.js
@@ -6,16 +6,19 @@ import UserRepository from './UserRepository';
 import User from './User';
 
 const userRepository = new UserRepository(userData);
+const user = new User(userRepository.getUserData(33));
+const randomUser = new User(userData[Math.floor(Math.random() * userData.length)]);
+console.log(randomUser);
 
 const welcomeMessage = document.querySelector('h2');
-const user = new User(userRepository.userData[22]);
 
 const generateWelcomeMessage = () => {
-  console.log(userRepository);
-  console.log(user);
-  welcomeMessage.innerText = `Welcome, ${user.returnUserFirstName()}`;
+  welcomeMessage.innerText = `Welcome, ${randomUser.returnUserFirstName()}`;
 };
 
 window.addEventListener('load', (event) => {
   generateWelcomeMessage();
 });
+
+// userData is an array of objects
+// userRepository is an object with all of the users in an array
