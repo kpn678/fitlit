0eef9f8971ca5029d4c93754983292f9eb57f12d
Grant Beard <Grantxbeard@gmail.com>
2022-05-18 14:52:12 -0600

    create fetch function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,8 @@
 // Your fetch requests will live here!
+const fetchData = (dataSet) => {
+  return fetch(`https://fitlit-api.herokuapp.com/api/v1/${dataSet}`)
+    .then((resopnse) => response.json())
+    .catch((err) => console.log("There has been an error!"));
+};
 
-
-console.log('I will be a fetch request!')
-
+console.log("I will be a fetch request!");
--- src/scripts.js
+++ src/scripts.js
@@ -1,43 +1,53 @@
-import './css/styles.css';
-import './images/turing-logo.png'
-import userData from './data/users';
+import "./css/styles.css";
+import "./images/turing-logo.png";
+import userData from "./data/users";
 
-import UserRepository from './UserRepository';
-import User from './User';
+import UserRepository from "./UserRepository";
+import User from "./User";
 
 const userRepository = new UserRepository(userData);
 const user = new User(userRepository.getUserData(33));
-const randomUser = new User(userData[Math.floor(Math.random() * userData.length)]);
+const randomUser = new User(
+  userData[Math.floor(Math.random() * userData.length)]
+);
 console.log(randomUser);
 
-const welcomeMessage = document.querySelector('h2');
-const openProfileButton = document.querySelector('.profile-button');
-const closeProfileButton = document.querySelector('.close-profile-button');
-const stepGoalDisplay = document.querySelector('#stepGoals');
-const accountInfo = document.querySelector('#accountInfo');
+const welcomeMessage = document.querySelector("h2");
+const openProfileButton = document.querySelector(".profile-button");
+const closeProfileButton = document.querySelector(".close-profile-button");
+const stepGoalDisplay = document.querySelector("#stepGoals");
+const accountInfo = document.querySelector("#accountInfo");
 
 const generateWelcomeMessage = () => {
   welcomeMessage.innerText = `Welcome back, ${randomUser.returnUserFirstName()}!`;
 };
 
 const displayStepGoal = () => {
-  stepGoalDisplay.innerHTML = `The average of all our users' daily step goals is: ${userRepository.calculateAverageStepGoals()} steps. <br> Your daily step goal is: ${randomUser.dailyStepGoal} steps. <br> Your stride length is: ${randomUser.strideLength} feet.`
+  stepGoalDisplay.innerHTML = `The average of all our users' daily step goals is: ${userRepository.calculateAverageStepGoals()} steps. <br> Your daily step goal is: ${
+    randomUser.dailyStepGoal
+  } steps. <br> Your stride length is: ${randomUser.strideLength} feet.`;
 };
 
 const displayAccountInfo = () => {
-  accountInfo.innerHTML = `Your Account Info <br><br> ${randomUser.name} <br><br> ${randomUser.email} <br><br> ${randomUser.address} <br><br> Your Friends Are: ${randomUser.returnFriendName(userData)}`
+  accountInfo.innerHTML = `Your Account Info <br><br> ${
+    randomUser.name
+  } <br><br> ${randomUser.email} <br><br> ${
+    randomUser.address
+  } <br><br> Your Friends Are: ${randomUser.returnFriendName(
+    userRepository.userData
+  )}`;
 };
 
-window.addEventListener('load', (event) => {
+window.addEventListener("load", (event) => {
   generateWelcomeMessage();
   displayStepGoal();
 });
 
-openProfileButton.addEventListener('click', (event) => {
-  overlay.style.display = 'block';
+openProfileButton.addEventListener("click", (event) => {
+  overlay.style.display = "block";
   displayAccountInfo();
 });
 
-closeProfileButton.addEventListener('click', (event) => {
-  overlay.style.display = 'none';
+closeProfileButton.addEventListener("click", (event) => {
+  overlay.style.display = "none";
 });
