3942e47df273e1874b6bdf67e617c740dd693cbf
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-05-19 13:56:14 -0600

    Add files and set up exports and imports

---
+++ test/Hydration-test.js
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import UserRepository from "../src/UserRepository";
+import User from "../src/User";
+import Hydtration from "../src/Hydtration";
+
+describe("Hydration", () => {
+
+
+    
+    let 
+    beforeEach(() => {
+      
+    });
+
+    it("should be a function", () => {
+      expect(Hydration).to.be.a("function");
+    });
+
+    it("should be an instance of Hydration", () => {
+      expect(SOMETHING).to.be.an.instanceof(Hydration);
+    });
+
+    
+})
