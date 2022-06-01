114578265130d297378715e0c7f54e6dc94480b7
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-06-01 17:45:42 -0600

    move user sample data to deparate file

---
+++ src/sampleData/user-sample-data.js
@@ -0,0 +1,72 @@
+export const userData = [{
+    id: 31,
+    name: "Bert Yund",
+    address: "0032 Claudia Plain, Delfinaland RI 22298-3685",
+    email: "Sibyl.Schmidt39@yahoo.com",
+    strideLength: 3.4,
+    dailyStepGoal: 7000,
+    friends: [32, 33],
+},
+ {
+    id: 32,
+    name: undefined,
+    address: "408 Windler Camp, Eddietown MA 11960",
+    email: "Nikolas.Brakus31@yahoo.com",
+    strideLength: 4.3,
+    dailyStepGoal: 3000,
+    friends: [31, 32],
+},
+ {
+    id: 33,
+    name: "Lani Quitt",
+    address: "60013 Golden Overpass, Lake Dejon WI 77309-0820",
+    email: "Trinity_Rowe@hotmail..com",
+    strideLength: 3.5,
+    dailyStepGoal: 5000,
+    friends: [],
+}]
+
+export const hydrationData = [
+    {
+        userID: 32,
+        date: "2019/06/15",
+        numOunces: 51,
+    },
+    {
+        userID: 33,
+        date: "2019/06/16",
+        numOunces: 89,
+    },
+    {
+        userID: 33,
+        date: "2019/06/17",
+        numOunces: 23,
+    },
+];
+
+export const sleepData = [
+    {
+        userID: 33,
+        date: "2019/06/15",
+        hoursSlept: 9.5,
+        sleepQuality: 4.2,
+    },
+    {
+        userID: 33,
+        date: "2019/06/16",
+        hoursSlept: 10.1,
+        sleepQuality: 3.8,
+    },
+    {
+        userID: 32,
+        date: "2019/06/16",
+        hoursSlept: 7,
+        sleepQuality: 4.8,
+    },
+    {
+        userID: 32,
+        date: "2019/06/17",
+        hoursSlept: 9.1,
+        sleepQuality: 1.9,
+    },
+];
--- test/User-test.js
+++ test/User-test.js
@@ -3,91 +3,22 @@ import UserRepository from "../src/UserRepository";
 import User from "../src/User";
 import Hydration from "../src/Hydration";
 import Sleep from "../src/Sleep";
+import {userData, hydrationData, sleepData} from "../src/sampleData/user-sample-data";
+import { activityData } from "../src/sampleData/activity-sample-data";
+import Activity from "../src/Activity";
 
 describe("User", () => {
-  let user1, user2, user3, user31, user32, user33, hydrationData, sleepData, userData, userRepository, hydration, sleep;
+  let  user31, user32, user33, userRepository, hydration, sleep, activity;
 
   beforeEach(() => {
-    user1 = {
-      id: 31,
-      name: "Bert Yund",
-      address: "0032 Claudia Plain, Delfinaland RI 22298-3685",
-      email: "Sibyl.Schmidt39@yahoo.com",
-      strideLength: 3.4,
-      dailyStepGoal: 7000,
-      friends: [32, 33],
-    };
-    user2 = {
-      id: 32,
-      name: undefined,
-      address: "408 Windler Camp, Eddietown MA 11960",
-      email: "Nikolas.Brakus31@yahoo.com",
-      strideLength: 4.3,
-      dailyStepGoal: 3000,
-      friends: [31, 32],
-    };
-    user3 = {
-      id: 33,
-      name: "Lani Quitt",
-      address: "60013 Golden Overpass, Lake Dejon WI 77309-0820",
-      email: "Trinity_Rowe@hotmail..com",
-      strideLength: 3.5,
-      dailyStepGoal: 5000,
-      friends: [],
-    };
 
-    hydrationData = [
-      {
-        userID: 32,
-        date: "2019/06/15",
-        numOunces: 51,
-      },
-      {
-        userID: 33,
-        date: "2019/06/16",
-        numOunces: 89,
-      },
-      {
-        userID: 33,
-        date: "2019/06/17",
-        numOunces: 23,
-      },
-    ];
-
-    sleepData = [
-      {
-        userID: 33,
-        date: "2019/06/15",
-        hoursSlept: 9.5,
-        sleepQuality: 4.2,
-      },
-      {
-        userID: 33,
-        date: "2019/06/16",
-        hoursSlept: 10.1,
-        sleepQuality: 3.8,
-      },
-      {
-        userID: 32,
-        date: "2019/06/16",
-        hoursSlept: 7,
-        sleepQuality: 4.8,
-      },
-      {
-        userID: 32,
-        date: "2019/06/17",
-        hoursSlept: 9.1,
-        sleepQuality: 1.9,
-      },
-    ];
-
-    user31 = new User(user1);
-    user32 = new User(user2);
-    user33 = new User(user3, hydration, sleep);
-    userData = [user1, user2, user3];
+    user31 = new User(userData[0]);
+    user32 = new User(userData[1]);
+    user33 = new User(userData[2], hydration, sleep, activity);
     userRepository = new UserRepository(userData);
     hydration = new Hydration(hydrationData, 33);
     sleep = new Sleep(sleepData, 33);
+    activity = new Activity(activityData, 33)
   });
 
   it("should be a function", () => {
@@ -98,75 +29,12 @@ describe("User", () => {
     expect(user33).to.be.an.instanceof(User);
   });
 
-  it.skip("should represent a single user's data", () => {
-    expect(user33).to.deep.equal({
-      id: 33,
-      name: "Lani Quitt",
-      address: "60013 Golden Overpass, Lake Dejon WI 77309-0820",
-      email: "Trinity_Rowe@hotmail..com",
-      strideLength: 3.5,
-      dailyStepGoal: 5000,
-      friends: [],
-      hydrationData: {
-        hydrationData: [
-          {
-            userID: 33,
-            date: "2019/06/16",
-            numOunces: 89,
-          },
-          {
-            userID: 33,
-            date: "2019/06/17",
-            numOunces: 23,
-          },
-        ],
-        userID: 33,
-      },
-      sleepData: {
-        allUserSleepData:[
-          {
-            userID: 33,
-            date: "2019/06/15",
-            hoursSlept: 9.5,
-            sleepQuality: 4.2,
-          },
-          {
-            userID: 33,
-            date: "2019/06/16",
-            hoursSlept: 10.1,
-            sleepQuality: 3.8,
-          },
-          {
-            userID: 32,
-            date: "2019/06/16",
-            hoursSlept: 7,
-            sleepQuality: 4.8,
-          },
-          {
-            userID: 32,
-            date: "2019/06/17",
-            hoursSlept: 9.1,
-            sleepQuality: 1.9,
-          },
-      ],
-
-        sleepData: [
-          {
-            userID: 33,
-            date: "2019/06/15",
-            hoursSlept: 9.5,
-            sleepQuality: 4.2,
-          },
-          {
-            userID: 33,
-            date: "2019/06/16",
-            hoursSlept: 10.1,
-            sleepQuality: 3.8,
-          },
-        ],
-        userID: 33,
-      },
-    });
+  it("should represent a single user's data", () => {
+    expect(user33.id).to.deep.equal(33);
+    expect(user33.email).to.equal("Trinity_Rowe@hotmail..com");
+    expect(user33.strideLength).to.equal(3.5);
+    expect(user33.dailyStepGoal).to.equal(5000);
+    expect(user33.friends).to.deep.equal([]);
   });
 
   it("should be able to store a user's hydration data", () => {
@@ -175,6 +43,10 @@ describe("User", () => {
 
   it("should be able to store a user's sleep data", () => {
     expect(user33.sleepData).to.deep.equal(sleep);
+  });
+
+  it("should be able to store a user's activity data", () => {
+    expect(user33.activityData).to.deep.equal(activity);
   });
 
   it("should be able to return a user's first name", () => {
